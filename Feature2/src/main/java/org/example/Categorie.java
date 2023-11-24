fa432536097dbee7dc8325823623d7ead6fc6598
amelboursali <boursali.amel@gmail.com>
2023-11-24 10:24:26 +0100

    ABB-Ajout du squelette de code pour le feature 2

---
+++ Feature2/src/main/java/org/example/Categorie.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public enum Categorie {
+    Boucherie,
+    Poissonerie,
+    Primeur,
+    Supermarché,
+    Hypermarché,
+    Superette,
+    Alimentation_Générale,
+    Epiecreie,
+    Boulangerie;
+}
---
+++ Feature2/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+
+    Magasin mag1 = new Magasin("Soouest", "Leclerc", Categorie.Supermarché );
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
