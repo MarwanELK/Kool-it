d8f15855dc956d047a385f646a0a9f8dfc46a3c8
amelboursali <boursali.amel@gmail.com>
2023-11-26 22:24:37 +0100

    Supprimer des fichiers et dossiers

--- Kool-it-master 5/backend_koolit/src/main/java/project/spring/backend_koolit/repository/RecetteRepository.java
+++
@@ -1,7 +0,0 @@
-package project.spring.backend_koolit.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.spring.backend_koolit.model.Recette;
-
-public interface RecetteRepository extends JpaRepository<Recette, Long> {
-}
-
