b89c1e074fd5f40eede5b5a0112612fe23267fef
Ayoubkhayef <106162115+Ayoubkhayef@users.noreply.github.com>
2023-12-13 00:53:31 +0100

    Feature 1
    Ajout du boutons "Ajouter tous à la liste"

--- frontend_koolit/src/app/produits/produits.component.ts
+++ frontend_koolit/src/app/produits/produits.component.ts
@@ -1,6 +1,7 @@
 // produits.component.ts
 import { Component, OnInit } from '@angular/core';
-import { RecetteService } from './recette.service'; // Assurez-vous que le chemin est correct
+import { RecetteService } from './recette.service';
+import { KoolitService } from './koolit.service';
 
 @Component({
   selector: 'app-produits',
@@ -10,7 +11,7 @@ import { RecetteService } from './recette.service'; // Assurez-vous que le chemi
 export class ProduitsComponent implements OnInit {
   recettes: any[] = [];
 
-  constructor(private recetteService: RecetteService) {}
+  constructor(private recetteService: RecetteService, private koolitService: KoolitService) {}
 
   ngOnInit(): void {
     this.recetteService.getRecettes().subscribe(
@@ -20,6 +21,47 @@ export class ProduitsComponent implements OnInit {
       },
       (error) => {
         console.error('Erreur lors de la récupération des recettes :', error);
+      }
+    );
+  }
+
+  ajouterALaListeDeCourses(ingredient: any): void {
+   
+    const ingredientAEnvoyer = {
+      nom: ingredient.nom,
+      type: ingredient.type,
+    };
+  
+    const nouvelleListe = {
+      utilisateurId: 5,  
+      ingredients: JSON.stringify([ingredientAEnvoyer]),
+    };
+  
+    this.koolitService.ajouterIngredient(5, nouvelleListe).subscribe(
+      (response: any) => {
+        console.log('Ingrédient ajouté avec succès dans la base de données :', response);
+       
+        this.chargerListeDeCourses(5); 
+      },
+      (error) => {
+        console.error('Erreur lors de l\'ajout de l\'ingrédient dans la base de données :', error);
+      }
+    );
+  }
+  ajouterTousALaListeDeCourses(recette: any): void {
+    for (const ingredient of recette.ingredients) {
+      this.ajouterALaListeDeCourses(ingredient);
+    }
+  }
+ 
+  private chargerListeDeCourses(utilisateurId: number): void {
+    this.koolitService.getListeDeCourses(utilisateurId).subscribe(
+      (listeCoursesData: any[]) => {
+        console.log('Données de la liste de courses reçues du backend :', listeCoursesData);
+        
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération de la liste de courses :', error);
       }
     );
   }
