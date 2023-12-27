8d7f54435dcfab41e0d734ca99d12a98feaf211c
MarwanELK <marwan_elkhaddar@hotmail.fr>
2023-12-27 15:11:56 +0100

    verif

--- frontend_koolit/src/app/app-routing.module.ts
+++ frontend_koolit/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ContactComponent } from './contact/contact.component';
 import { ProduitsListComponent } from './produits-list/produits-list.component';
 import { ListeDeCoursesComponent } from './liste-de-courses/liste-de-courses.component';
 import { MagasinsComponent } from './magasins/magasins.component';
+import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'produits-list', component: ProduitsListComponent }, 
   { path: 'liste-de-courses', component: ListeDeCoursesComponent },
-  { path: 'Magasins', component: MagasinsComponent },
+  { path: 'magasins', component: MagasinsComponent },
+  { path: 'map', component: MapComponent},
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: '**', redirectTo: '/accueil' }
 ];
