6a74f848551fe5460d0ab2e2c330e31a0236f9cc
MarwanELK <marwan_elkhaddar@hotmail.fr>
2023-12-27 15:09:10 +0100

    suppression classes inutiles

--- backend_koolit/src/main/java/project/spring/backend_koolit/model/Magasin.java
+++ backend_koolit/src/main/java/project/spring/backend_koolit/model/Magasin.java
@@ -23,6 +23,11 @@ public class Magasin {
     @Column(name = "type_aliment")
     private String typeAliment;
 
+    @Column(name = "latitude")
+    private double lat;
+    @Column(name = "longitude")
+    private double lon;
+
     public Magasin() {
         // Constructeur par défaut nécessaire pour JPA
     }
@@ -34,7 +39,32 @@ public class Magasin {
         this.typeAliment = typeAliment;
     }
 
-    // Getters et setters
+    public Magasin(Long id, String nom, String typeMagasin, String urlMagasin, String typeAliment, double lat, double lon) {
+        this.id = id;
+        this.nom = nom;
+        this.typeMagasin = typeMagasin;
+        this.urlMagasin = urlMagasin;
+        this.typeAliment = typeAliment;
+        this.lat = lat;
+        this.lon = lon;
+    }
+// Getters et setters
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
 
     public Long getId() {
         return id;
