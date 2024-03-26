de45c61c9aaf16677821cc230d833ae0b95a2235
MarwanELK <marwan_elkhaddar@hotmail.fr>
2024-03-26 14:34:37 +0100

    clean test front

--- frontend_koolit/src/app/koolit.service.spec.ts
+++ frontend_koolit/src/app/koolit.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; 
 import { KoolitService } from './koolit.service';
 
 describe('KoolitService', () => {
   let service: KoolitService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Importez les modules nécessaires
+      providers: [KoolitService] // Ajoutez le service dans le tableau providers
+    });
     service = TestBed.inject(KoolitService);
   });
 
--- frontend_koolit/src/app/wikingredients/wikingredients.component.spec.ts
+++ frontend_koolit/src/app/wikingredients/wikingredients.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Importez HttpClientTestingModule
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importez FormsModule
 import { WikingredientsComponent } from './wikingredients.component';
 
 describe('WikingredientsComponent', () => {
@@ -8,7 +11,8 @@ describe('WikingredientsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [WikingredientsComponent]
+      declarations: [WikingredientsComponent],
+      imports: [HttpClientTestingModule, CommonModule,FormsModule]
     });
     fixture = TestBed.createComponent(WikingredientsComponent);
     component = fixture.componentInstance;
